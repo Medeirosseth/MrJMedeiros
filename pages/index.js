51a483b9eda662b5685d5fda0783e5d593be890b
Seth <Medeirosseth@gamil.com>
2022-10-20 11:29:01 -0700

    dark mode

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { BsFillMoonStarsFill } from "react-icons/bs";
+import { BiStoreAlt } from "react-icons/bi";
 import {
   AiFillTwitterCircle,
   AiFillInstagram,
@@ -13,44 +14,41 @@ import jNoBG from "../public/jNoBG.png";
 import Alltta from "../public/Alltta.jpg";
 import TBK from "../public/TBK.png";
 import Procussions from "../public/Procussions.jpg";
-import React, { useState, useEffect } from "react";
+import mainPhoto from "../public/mainPhoto.jpg";
+import { useState } from "react";
 
 const YOUTUBE_PLAYLIST_ITEMS_API =
   "https://www.googleapis.com/youtube/v3/playlistItems";
 
 export async function getServerSideProps() {
   //Alltta
-  const res = await fetch(
-    `${YOUTUBE_PLAYLIST_ITEMS_API}?part=snippet&playlistId=PLF3lu5b2iqGUIbDk3k-z3AWL2HUc2hsTP&maxResults=30&key=${process.env.YOUTUBE_API_KEY}`
-  );
-
-  const data = await res.json();
+  const [allttaRes, procussionsRes] = await Promise.all([
+    fetch(
+      `${YOUTUBE_PLAYLIST_ITEMS_API}?part=snippet&playlistId=PLF3lu5b2iqGUIbDk3k-z3AWL2HUc2hsTP&maxResults=30&key=${process.env.YOUTUBE_API_KEY}`
+    ),
+    fetch(
+      `${YOUTUBE_PLAYLIST_ITEMS_API}?part=snippet&playlistId=PLXDcgC-3o4rokijUgx_dF_zhxEkghssT5&maxResults=6&key=${process.env.YOUTUBE_API_KEY}`
+    ),
+  ]);
+  const [alltta, procussions] = await Promise.all([
+    allttaRes.json(),
+    procussionsRes.json(),
+  ]);
   return {
     props: {
-      data,
+      alltta,
+      procussions,
     },
   };
 }
 
-export async function getServerSidePropsTwo() {
-  //The Procussions
-  const res = await fetch(
-    `${YOUTUBE_PLAYLIST_ITEMS_API}?part=snippet&playlistId=PLXDcgC-3o4rokijUgx_dF_zhxEkghssT5&maxResults=6&key=${process.env.YOUTUBE_API_KEY}`
-  );
+export default function Home({ alltta, procussions }) {
+  const [darkMode, setDarkMode] = useState(false);
 
-  const dataOne = await res.json();
-  return {
-    props: {
-      dataOne,
-    },
-  };
-}
-
-export default function Home({ data, dataOne }) {
-  console.log(data);
-  console.log("THE PROCUSSIONS : ", dataOne);
+  console.log(alltta);
+  console.log("THE PROCUSSIONS : ", procussions);
   return (
-    <div className={styles.container}>
+    <div className={darkMode ? "dark" : ""}>
       <Head>
         <title>Mr J</title>
 
@@ -58,17 +56,20 @@ export default function Home({ data, dataOne }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="bg-white px-10">
+      <main className="bg-white px-10 dark:bg-gray-600">
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
-            <h1 className="text-xl"> Mr J</h1>
+            <h1 className="text-xl "> Mr J</h1>
             <ul className="flex items-center">
               <li>
-                <BsFillMoonStarsFill className="cursor-pointer 2-xl" />
+                <BsFillMoonStarsFill
+                  onClick={() => setDarkMode(!darkMode)}
+                  className="cursor-pointer 2-xl"
+                />
               </li>
               <li>
                 <a
-                  className="bg-gradient-to-r from-black to-black text-white px-4 py-2 rounded-md ml-8"
+                  className="bg-gradient-to-r from-yellow to-black text-black px-4 py-2 rounded-md ml-8"
                   href="https://genius.com/search?q=Mrj%20Medeiross"
                 >
                   Rap Genius
@@ -94,20 +95,44 @@ export default function Home({ data, dataOne }) {
               <AiFillYoutube />
             </a>
           </div>
-          <div className="relative mx-auto bg-gradient-to-t from-white to-black rounded-full  mt-20">
+          <div className="flex justify-center">
             <Image className="rounded-full w-200 h-200 " src={jWithBG} />
           </div>
         </section>
-
         <section>
-          <div>
+          <div className="flex justify-center">
             <h3 className="text-3xl py-1">Works</h3>
           </div>
           <div>
             <div className="text-center shadow-lg p-10 rounded-xl my-10">
               <Image src={Alltta} width={100} height={100} />
+              <ui>
+                {alltta.items.map((item) => {
+                  const { id, snippet = {} } = item;
+                  const { title, thumbnails = {} } = snippet;
+                  const { medium = {} } = thumbnails;
+                  return (
+                    <li key={id} className="card flex flex-col justify-center">
+                      <p>
+                        <img
+                          width={medium.width}
+                          height={medium.height}
+                          src={medium.url}
+                        />
+                      </p>
+                      <h3>{title}</h3>
+                    </li>
+                  );
+                })}
+              </ui>
+            </div>
+          </div>
+          <div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10">
+              <Image src={Procussions} width={100} height={100} />
+
               <ui className="grid list-none grid-rows-2 text-center flex items-center">
-                {data.items.map((item) => {
+                {procussions.items.map((item) => {
                   const { id, snippet = {} } = item;
                   const { title, thumbnails = {} } = snippet;
                   const { medium = {} } = thumbnails;
@@ -129,21 +154,56 @@ export default function Home({ data, dataOne }) {
           </div>
           <div>
             <div className="text-center shadow-lg p-10 rounded-xl my-10">
-              <Image src={TBK} width={100} height={100} />
-              <h3>ALLTTA</h3>
-            </div>
-          </div>
-          <div>
-            <div className="text-center shadow-lg p-10 rounded-xl my-10">
-              <Image src={Procussions} width={100} height={100} />
               <h3>ALLTTA</h3>
             </div>
           </div>
         </section>
-        <section>
-          <h3>Section for live shows</h3>
-        </section>
+        <section></section>
       </main>
     </div>
   );
 }
+
+// <div id="carouselExampleControls" class="carousel slide relative" data-bs-ride="carousel">
+//   <div class="carousel-inner relative w-full overflow-hidden">
+//     <div class="carousel-item active relative float-left w-full">
+//       <img
+//         src="https://mdbcdn.b-cdn.net/img/new/slides/041.webp"
+//         class="block w-full"
+//         alt="Wild Landscape"
+//       />
+//     </div>
+//     <div class="carousel-item relative float-left w-full">
+//       <img
+//         src="https://mdbcdn.b-cdn.net/img/new/slides/042.webp"
+//         class="block w-full"
+//         alt="Camera"
+//       />
+//     </div>
+//     <div class="carousel-item relative float-left w-full">
+//       <img
+//         src="https://mdbcdn.b-cdn.net/img/new/slides/043.webp"
+//         class="block w-full"
+//         alt="Exotic Fruits"
+//       />
+//     </div>
+//   </div>
+//   <button
+//     class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
+//     type="button"
+//     data-bs-target="#carouselExampleControls"
+//     data-bs-slide="prev"
+//   >
+//     <span class="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+//     <span class="visually-hidden">Previous</span>
+//   </button>
+//   <button
+//     class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
+//     type="button"
+//     data-bs-target="#carouselExampleControls"
+//     data-bs-slide="next"
+//   >
+//     <span class="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+//     <span class="visually-hidden">Next</span>
+//   </button>
+// </div>
